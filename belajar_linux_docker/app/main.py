00af405439f980d188a3db8335981f84666cf0ad
barondra <bharindra.kamanditya@gmail.com>
2025-03-10 17:11:30 +0700

    added belajar_docker_linux directory

---
+++ belajar_linux_docker/app/main.py
@@ -0,0 +1,4 @@
+print("hello world in app/main.py")
+print("hello world v2 in app/main.py")
+print("hello world mounting in app/main.py")
+print('halo')
---
+++ belajar_linux_docker/app/test.py
@@ -0,0 +1,2 @@
+print("ini cuman testing dari test.py")
+print("sekarang versi baru dari test.py")
